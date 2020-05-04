c633a81adf309257be3f7413d7527f49be92ad8b
lehdonen <leehtonen@gmail.com>
2020-05-04 20:42:31 +0300

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ForecastComponent } from './components/forecast/forecast.component';
+import { CurrentComponent } from './components/current/current.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'forecast',
+  component: ForecastComponent
+},
+{
+  path: 'current',
+  component: CurrentComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
