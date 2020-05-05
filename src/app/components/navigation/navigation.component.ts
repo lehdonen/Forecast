87ee59fd3cfd2a67b3b209c2d18f51a4fc416aa4
lehdonen <leehtonen@gmail.com>
2020-05-05 21:51:20 +0300

    Add navigation functionality

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface NavigationPath {
+  path: string;
+  label: string;
+  active: boolean;
+}
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +14,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  navigationPaths: NavigationPath[] = [
+    {
+      path: '',
+      label: 'home',
+      active: false,
+    },
+    {
+      path: 'forecast',
+      label: 'forecast',
+      active: false,
+    }
+    ];
+
+  constructor(
+    private router: Router,
+  ) {  }
 
   ngOnInit() {
+    const currentPath = window.location.pathname;
+    this.navigationPaths = this.navigationPaths.map(navPath => ({
+      ...navPath,
+      active: currentPath === navPath.path,
+    }));
+  }
+
+  navigate(path: NavigationPath) {
+    this.router.navigate([path.path]);
+    this.navigationPaths = this.navigationPaths.map(navPath => ({
+      ...navPath,
+      active: navPath.path === path.path,
+    }));
   }
 
 }
