dcc79ba93e5a2bdc100453ca3dad77645924fe85
lehdonen <leehtonen@gmail.com>
2020-05-18 20:11:32 +0300

    Add map and search bar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherDTO } from 'src/app/models/Weather';
 import { ApiService } from 'src/app/services/api.service';
+import { CityService } from 'src/app/services/city.service';
 
 @Component({
   selector: 'app-home',
@@ -9,20 +10,44 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class HomeComponent implements OnInit {
 
-  city: 'Helsinki';
+  city: string;
   currentWeather: WeatherDTO;
   weatherIconURL: string;
+  center: google.maps.LatLngLiteral;
 
   constructor(
-    private apiService: ApiService
+    private apiService: ApiService,
+    private cityService: CityService
   ) {
   }
 
   ngOnInit() {
-    this.apiService.getWeather('Helsinki').subscribe((res: WeatherDTO) => {
+    if (this.cityService.getCity() === undefined) {
+      this.cityService.changeCity('Helsinki');
+    }
+    this.city = this.cityService.getCity();
+    this.apiService.getWeather(this.cityService.getCity()).subscribe((res: WeatherDTO) => {
+      this.center = {
+        lat: res.coord.lat,
+        lng: res.coord.lon,
+      };
+      this.currentWeather = res;
+      this.weatherIconURL = `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
+    });
+  }
+
+  search(input: string) {
+    this.cityService.changeCity(input);
+    this.city = this.cityService.getCity();
+
+    this.apiService.getWeather(this.city).subscribe((res: WeatherDTO) => {
       setTimeout(() => {
         this.currentWeather = res;
         this.weatherIconURL = `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
+        this.center = {
+          lat: res.coord.lat,
+          lng: res.coord.lon,
+        };
       }, 1000);
     });
   }
