4f87b2bfe00bd91dc3bbf5aaa4bb42e14deb78da
lehdonen <leehtonen@gmail.com>
2020-05-18 20:11:50 +0300

    Add city from service to forecast

--- src/app/components/forecast/forecast.component.ts
+++ src/app/components/forecast/forecast.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { ForecastDTO } from 'src/app/models/Forecast';
+import { CityService } from 'src/app/services/city.service';
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
   styleUrls: ['./forecast.component.scss']
 })
 export class ForecastComponent implements OnInit {
-
+  city: string;
   forecast: ForecastDTO;
   iconUrl = 'https://openweathermap.org/img/wn/';
 
   constructor(
-    private apiService: ApiService
+    private apiService: ApiService,
+    private cityService: CityService
   ) { }
 
   ngOnInit() {
-    this.apiService.getForecast('Helsinki').subscribe(res => {
+    this.city = this.cityService.getCity();
+    console.log(this.city);
+
+    this.apiService.getForecast(this.city).subscribe(res => {
       setTimeout(() => {
         this.forecast = res;
       }, 1000);
